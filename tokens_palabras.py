19c2f62010716daa0efb4397f5421f7e8cea2097
Josh Sebastián López Murcia <joshlopezmurcia@gmail.com>
2025-06-17 00:27:20 -0500

    File Input

---
+++ tokens_palabras.py
@@ -0,0 +1,65 @@
+# Diccionario de librerías y sus palabras reservadas
+palabras_reservadas_librerias = {
+    # Módulo math
+    'math',
+
+    # Módulo os
+    'os',
+
+    # Módulo sys
+    'sys',
+
+    # Módulo datetime
+    'datetime',
+
+    # Módulo random
+    'random',
+
+    # Módulo re (Expresiones Regulares)
+    're',
+
+    # Librería numpy
+    'numpy',
+
+    # Librería pandas
+    'pandas',
+
+    # Librería matplotlib.pyplot
+    'matplotlib',
+
+    # Librería sklearn
+    'sklearn',
+
+    # Flask
+    'flask',
+
+    # Django
+    'django',
+
+    # FastAPI
+    'fastapi',
+
+    # SQL (usado en sqlite3, MySQL, etc.)
+    'sql',
+
+    # MongoDB (pymongo)
+    'pymongo',
+
+    # asyncio
+    'asyncio',
+
+    # Manejo de archivos (open)
+    'file',
+
+    # JSON
+    'json' ,
+
+    # XML
+    'xml',
+
+    # Requests (para peticiones HTTP)
+    'requests',
+
+    # Sockets
+    'socket'
+}
